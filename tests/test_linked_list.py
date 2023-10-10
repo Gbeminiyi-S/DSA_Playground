be1323c34369f77ba3ed5115bf383c0eb69f881e
Gbeminiyi2022 <samagbeyigbeminiyi@gmail.com>
2023-10-10 10:38:20 +0100

    adding test case for insert at index

--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -18,13 +18,37 @@ class TestLinkedList(unittest.TestCase):
         - Second, insert another node and verify that it becomes the new head of the list.
         """
         # first insert in list
-        self.linked_list.insert_at_beginning(42)
-        self.assertEqual(self.linked_list.head.data, 42)
+        data = 42
+        self.linked_list.insert_at_beginning(data)
+        self.assertEqual(self.linked_list.head.data, data)
         self.assertIsNone(self.linked_list.head.next)
 
         # second insert in list
-        self.linked_list.insert_at_beginning(95)
-        self.assertEqual(self.linked_list.head.data, 95)
+        data = 95
+        self.linked_list.insert_at_beginning(data)
+        self.assertEqual(self.linked_list.head.data, data)
+
+        # third insert in list
+        data = 55
+        self.linked_list.insert_at_beginning(data)
+        self.assertEqual(self.linked_list.head.data, data)
+
+    def test_insertAtIndex(self) -> None:
+        self.test_insert_at_beginning()
+        index, data = 2, 101
+        self.linked_list.insertAtIndex(data, index)
+
+        current_node = self.getposition(index)
+        self.assertEqual(current_node.data, data)
+
+    def getposition(self, index:int):
+        current_node = self.linked_list.head
+        position = 0
+        while position != index:
+            current_node = current_node.next
+            position += 1
+        return current_node
+
 
 if __name__ == '__main__':
     unittest.main()
